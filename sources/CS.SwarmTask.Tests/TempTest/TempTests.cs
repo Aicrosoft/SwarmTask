107debddb01e8683797ceb01ccc6c22e9845cb30
atwind <atwind@cszi.com>
2016-05-19 19:48:12 +0800

    test

--- sources/CS.SwarmTask.Tests/TempTest/TempTests.cs
+++ sources/CS.SwarmTask.Tests/TempTest/TempTests.cs
@@ -29,35 +29,35 @@ namespace CS.SwarmTask.Tests.TempTest
         [Test]
         public void MemeryAlingTest()
         {
-            Console.WriteLine(Marshal.SizeOf(typeof (StructDeft)));
-            Console.WriteLine(Marshal.SizeOf(typeof (BadStruct)));
-            Console.WriteLine(Marshal.SizeOf(typeof (AutoStruct)));
+            //Console.WriteLine(Marshal.SizeOf(typeof (StructDeft)));
+            //Console.WriteLine(Marshal.SizeOf(typeof (BadStruct)));
+            //Console.WriteLine(Marshal.SizeOf(typeof (AutoStruct)));
 
             Assert.IsEmpty("");
         }
 
-        //[StructLayout(LayoutKind.Sequential, Pack = 1)] //这个就是16占用 了
-        private struct StructDeft //C#编译器会自动在上面运用[StructLayout(LayoutKind.Sequential)]  //会自动按最大的类型的字节数来对齐,所以是24
-        {
-            private bool i; //1Byte
-            private double c; //8byte
-            private bool b; //1byte
-        }
+        ////[StructLayout(LayoutKind.Sequential, Pack = 1)] //这个就是16占用 了
+        //private struct StructDeft //C#编译器会自动在上面运用[StructLayout(LayoutKind.Sequential)]  //会自动按最大的类型的字节数来对齐,所以是24
+        //{
+        //    private bool i; //1Byte
+        //    private double c; //8byte
+        //    private bool b; //1byte
+        //}
 
-        [StructLayout(LayoutKind.Explicit)]
-        private struct BadStruct
-        {
-            [FieldOffset(0)] public bool i; //1Byte
-            [FieldOffset(0)] public double c; //8byte
-            [FieldOffset(0)] public bool b; //1byte
-        }
+        //[StructLayout(LayoutKind.Explicit)]
+        //private struct BadStruct
+        //{
+        //    [FieldOffset(0)] public bool i; //1Byte
+        //    [FieldOffset(0)] public double c; //8byte
+        //    [FieldOffset(0)] public bool b; //1byte
+        //}
 
-        [StructLayout(LayoutKind.Auto)]
-        private struct AutoStruct
-        {
-            public bool i; //1Byte
-            public double c; //8byte
-            public bool b; //1byte
-        }
+        //[StructLayout(LayoutKind.Auto)]
+        //private struct AutoStruct
+        //{
+        //    public bool i; //1Byte
+        //    public double c; //8byte
+        //    public bool b; //1byte
+        //}
     }
 }
