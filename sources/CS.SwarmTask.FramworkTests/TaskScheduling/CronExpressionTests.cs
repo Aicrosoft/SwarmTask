7d6ac4dccf033788a7e2202175792afda2734b54
atwind <atwind@cszi.com>
2016-05-19 19:12:29 +0800

    项目结构重构

---
+++ sources/CS.SwarmTask.FramworkTests/TaskScheduling/CronExpressionTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CS.SwarmTask.FramworkTests.TaskScheduling
+{
+    [TestClass()]
+    public class CronExpressionTests
+    {
+        [TestMethod()]
+        public void CronExpressionTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
